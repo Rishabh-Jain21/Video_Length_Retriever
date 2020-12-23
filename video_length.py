5e282bddf864ac56dcad3a1feff5370c75c0f288
Rishabh <Rishabh9843@gmail.com>
2020-12-23 07:56:23 +0530

    Added command line functionality

--- video_length.py
+++ video_length.py
@@ -1,20 +1,25 @@
 import os
 import cv2
 import datetime
+import argparse
 
 
 def get_length(filename):
     cap = cv2.VideoCapture(filename)
-    # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     duration = frame_count/fps
     return duration
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', type=str, metavar='',
+                    required=True, help="Folder Path")
+
+args = parser.parse_args()
 total_time = float(0)
 
-start_path = r'H:\Course\Archive-5981\Online Account Cracking Course'
+start_path = args.path
 for dirpath, dirname, filenames in os.walk(start_path):
     for f in filenames:
         fp = os.path.join(dirpath, f)
