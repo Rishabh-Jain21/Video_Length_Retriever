292bd2479aeb680211a51d8530d159971cf81b66
Rishabh Jain <rishabh9843@gmail.com>
2023-04-15 15:32:38 +0530

    Added multithreading support for fast processing of videos

--- video_length.py
+++ video_length.py
@@ -2,13 +2,15 @@ import os
 import cv2
 import datetime
 import argparse
+from concurrent.futures import Future, ThreadPoolExecutor
+from typing import List
 
-
-def get_length(filename):
+# Function to get length of one video in seconds
+def get_length(filename)->float:
     cap = cv2.VideoCapture(filename)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    duration = frame_count/fps
+    duration:float = frame_count/fps
     return duration
 
 
@@ -17,15 +19,22 @@ parser.add_argument('-p', '--path', type=str, metavar='',
                     required=True, help="Folder Path")
 
 args = parser.parse_args()
-total_time = float(0)
-
-start_path = args.path
-for dirpath, dirname, filenames in os.walk(start_path):
-    for f in filenames:
-        fp = os.path.join(dirpath, f)
-        if fp.lower().endswith(('.avi', '.mpg', '.mpeg', '.wmv', '.mov', '.divx', '.mp4', '.m4v', '.asf')):
-            total_time += get_length(fp)
+total_time: float = 0.0  # Variable to store total time of all videos
 
 
-conversion = datetime. timedelta(seconds=total_time)
+start_path: str = args.path
+with ThreadPoolExecutor(max_workers=5) as executor:
+    processes: List[Future] = []
+    for dirpath, dirname, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            if fp.lower().endswith(('.avi', '.mpg', '.mpeg', '.wmv', '.mov', '.divx', '.mp4', '.m4v', '.asf')):
+                try:
+                    # appending the future object in a list which will be used later to add total time
+                    processes.append(executor.submit(get_length, fp))
+                except:
+                    pass
+time_lst: List[float] = list(map(lambda x: x.result(), processes))
+total_time = sum(time_lst) #adding all the vallues
+conversion = datetime.timedelta(seconds=total_time) # converting seconds to readable format
 print(conversion)
